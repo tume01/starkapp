ca55889828c4b3fd0f1fea28dca498d2cdcfcaee
Franco Tume <franco.tume@tektonlabs.com>
2016-05-06 10:05:00 -0500

    settings

--- bungalows/views.py
+++ bungalows/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from services.BungalowsService import BungalowsService
-# Create your views here.
 
 def index(request):
 
-    x = BungalowsService()
-
-    y = x.test()
+    x = Bungalowservice()
 
     return HttpResponse(y)
