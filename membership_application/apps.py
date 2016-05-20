8c58afeb1b67a342b6bc4b37d70a6751820a58c0
Zephiross <a20121129@pucp.pe>
2016-05-19 20:08:01 -0500

    back end MBR02 MBR03 MBR04
    hay cosas que no se pueden hacer por las dependencias con otras clases

--- membership_application/apps.py
+++ membership_application/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class Membership_applicationConfig(AppConfig):
+class MembershipApplicationConfig(AppConfig):
     name = 'membership_application'
--- membership_application/views.py
+++ membership_application/views.py
@@ -1,5 +1,3 @@
-from django.template import loader
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+
+# Create your views here.
