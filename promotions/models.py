99b5c2c06da64cea7b9e483b5054be46c3a71e7c
kvaleriano <evaleriano97@gmail.com>
2016-05-20 13:00:06 -0500

    Back de mantenimiento de promociones
    Añadí los models y repository, parece que los urls están rotos, falta
    revisar

--- promotions/models.py
+++ promotions/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.TextField(max_length=200)
+    discount_percentage = models.FloatField()
