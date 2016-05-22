06da76ed0fce1f66f313a5b8227856a24d25afc5
jose <jpereira@pucp.pe>
2016-05-21 22:29:25 -0500

    Front final de promociones
    Front final de promociones

--- promotions/urls.py
+++ promotions/urls.py
@@ -5,9 +5,9 @@ app_name = 'promotions'
 
 urlpatterns = [
     url(r'^create/insert', views.create_promotion, name='insert'),
-    url(r'^create', views.create_promotion, name='create_index'),
+    url(r'^create', views.create_index, name='create_index'),
     url(r'^edit/insert', views.edit_promotion, name='edit'),
-    url(r'^edit', views.edit_promotion, name='edit_index'),
+    url(r'^edit', views.edit_index, name='edit_index'),
     url(r'^delete', views.delete_promotion, name='delete'),
     url(r'^', views.index, name='index'),
 ]
