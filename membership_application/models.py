2a3a8a8c6e05f2c62f75aa1059588a01d3ae12df
Zephiross <a20121129@pucp.pe>
2016-05-23 17:44:36 -0500

    Agregar busqueda por filtro y correccion de campos
    Se agregó busqueda por id en algunas clases, busqueda por filtros y se
    cambiaron los campos de sol de memb

--- membership_application/models.py
+++ membership_application/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Membership_Application(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    dni = models.IntegerField()
     comments = models.CharField(max_length=200)
-    status=models.CharField(max_length=20)
+    initialDate = models.DateField()
+    finalDate = models.DateField()
+    status = models.CharField(max_length=20)
--- memberships/models.py
+++ memberships/models.py
@@ -8,4 +8,10 @@ class MembershipType(models.Model):
     price = models.FloatField()
     billing = models.TextField(max_length=200)
     status = models.IntegerField()
+
+class Membership(models.Model):
+    membership_type = models.ForeignKey(MembershipType, on_delete=models.CASCADE)
+    initialDate = models.DateField()
+    finalDate = models.DateField()
+    status = models.IntegerField()
     
