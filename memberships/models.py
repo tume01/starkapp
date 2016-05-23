b7f8b184afe058f0be268f1907a30be425944d80
kvaleriano <evaleriano97@gmail.com>
2016-05-23 11:44:24 -0500

    Back de tipos de Membresía
    También devolví la pass del database a root y arregle cosas de views y
    los templates

--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class MembershipType(models.Model):
+    name = models.TextField(max_length=200)
+    guests = models.IntegerField()
+    price = models.FloatField()
+    billing = models.TextField(max_length=200)
+    status = models.IntegerField()
+    
