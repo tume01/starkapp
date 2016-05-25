055ab7d8ee218f862d7dc69e27edf524aa28bb4c
jose <jpereira@pucp.pe>
2016-05-24 22:40:41 -0500

    Multas y usuarios
    Multas y usuarios

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     url(r'^type/edit/insert', views.edit_user_type, name='type/edit'),
     url(r'^type/edit', views.edit_user_type_index, name='type/edit_index'),
     url(r'^type', views.user_type_index, name='type/index'),
+    url(r'^edit/insert', views.edit_user, name='edit'),
+    url(r'^edit', views.edit_user_index, name='edit_index'),
+    url(r'^create/insert', views.create_user, name='insert'),
+    url(r'^create', views.create_user_index, name='create_index'),
+    url(r'^', views.user_index, name='index'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from services.UserTypeService import UsersTypeService
+from services.UsersService import UsersService
 from django.views.decorators.http import require_http_methods
 
 
@@ -74,3 +75,96 @@ def edit_user_type(request):
     user_type_service.update(id_edit, edit_data)
 
     return HttpResponseRedirect(reverse('users:type/index'))
+
+
+
+@require_http_methods(['POST'])
+def edit_user_index(request):
+
+    id_user = request.POST['id']
+
+    user_service = UsersService()
+
+    user = user_service.getUser(id_user)
+
+    user_type_service = UsersTypeService()
+
+    types = user_type_service.getTypes()
+
+    context = {
+        'user' : user,
+        'types' : types,
+    }
+
+    return render(request, 'Admin/Users/edit_user.html', context)
+
+@require_http_methods(['POST'])
+def edit_user(request):
+
+    edit_data = {}
+
+    edit_data["name"] = request.POST['name']
+
+    edit_data["password"] = request.POST['password']
+
+    edit_data["user_type_id"] = request.POST['user_type_id']
+
+    id_edit = request.POST['id']
+
+    user_service = UsersService()
+
+    user_service.update(id_edit, edit_data)
+
+    return HttpResponseRedirect(reverse('users:index'))
+
+
+@require_http_methods(['GET'])
+def create_user_index(request):
+
+    user_type_service = UsersTypeService()
+
+    types = user_type_service.getTypes()
+
+    context = {
+        'titulo' : 'titulo',
+        'types' : types,
+    }
+
+    return render(request, 'Admin/Users/new_user.html', context)
+
+
+@require_http_methods(['POST'])
+def create_user(request):
+
+    insert_data = {}
+
+    insert_data["name"] = request.POST['name']
+
+    insert_data["password"] = request.POST['password']
+
+    insert_data["user_type_id"] = request.POST['user_type_id']
+
+    user_service = UsersService()
+
+    user_service.create(insert_data)
+
+    return HttpResponseRedirect(reverse('members:index'))
+
+
+@require_http_methods(['GET'])
+def user_index(request):
+
+    user_service = UsersService()
+
+    users = user_service.getUsers()
+
+    user_type_service = UsersTypeService()
+
+    for user in users:
+        user.user_type_id = user_type_service.getType(user.user_type_id).description
+
+    context = {
+        'users' : users,
+    }
+
+    return render(request, 'Admin/Users/index_user.html', context) 
