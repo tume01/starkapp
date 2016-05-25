e9ba2ee2e1f942f2e8f5dc9058ee94182ed2d295
fabioolcese <a20110066@pucp.pe>
2016-05-25 18:45:49 -0400

    Forms validation
    Oliado y sacramentado

--- guests/forms.py
+++ guests/forms.py
@@ -6,5 +6,12 @@ class GuestForm(forms.Form):
 
     name = forms.CharField(max_length=200, validators=[alphabetic], error_messages={'required': 'El campo Nombres es requerido', 'max_length': 'El campo Nombres debe tener una longitud maxima de 200 caracteres'})
     surname = forms.CharField(max_length=200, validators=[alphabetic], error_messages={'required': 'El campo Apellidos es requerido', 'max_length': 'El campo Apellidos debe tener una longitud maxima de 200 caracteres'})
-    dni = forms.IntegerField(min_value=8, max_value=8, error_messages={'required': 'El campo DNI es requerido', 'min_value':'El campo DNI debe tener 8 digitos minimo', 'max_value':'El campo DNI debe tener 8 digitos como maximo'})
+    dni = forms.IntegerField(error_messages={'required': 'El campo DNI es requerido'})
 
+    def clean_dni(self):
+        data = self.cleaned_data['dni']
+        if (data < 9999999):
+            raise forms.ValidationError("El dni tiene que tener 8 digitos")
+        if (data > 100000000):
+            raise forms.ValidationError("El dni tiene que tener 8 digitos")
+        return data
