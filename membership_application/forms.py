e506e5fa5ed41e5b24381113ab8a8c2933375237
fabioolcese <a20110066@pucp.pe>
2016-05-24 21:51:42 -0400

    Validators
    Agregue algunos validators para asegurarnos que las cadenas sean solo de
    letras donde es conveniente.

--- membership_application/forms.py
+++ membership_application/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 class MembershipApplication(forms.Form):
-    firstName = forms.CharField(max_length=200, error_messages={'required': 'El campo Nombres es requerido', 'max_length': 'El campo Nombres debe tener una longitud maxima de 200 caracteres'})
-    lastName = forms.CharField(max_length=200, error_messages={'required': 'El campo Apellidos es requerido', 'max_length': 'El campo Apellidos debe tener una longitud maxima de 200 caracteres'})
+    alphabetic = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters are allowed.')
+
+    firstName = forms.CharField(max_length=200, validators=[alphabetic], error_messages={'required': 'El campo Nombres es requerido', 'max_length': 'El campo Nombres debe tener una longitud maxima de 200 caracteres'})
+    lastName = forms.CharField(max_length=200, validators=[alphabetic], error_messages={'required': 'El campo Apellidos es requerido', 'max_length': 'El campo Apellidos debe tener una longitud maxima de 200 caracteres'})
     dni = forms.IntegerField(min_value=8, max_value=8, error_messages={'required': 'El campo DNI es requerido', 'min_value':'El campo DNI debe tener 8 digitos minimo', 'max_value':'El campo DNI debe tener 8 digitos como maximo'})
     comments = forms.CharField(max_length=200, error_messages={'required': 'El campo Comentarios es requerido', 'max_length': 'El campo Comentarios debe tener una longitud maxima de 200 caracteres'})
     initialDate = forms.DateField(error_messages={'required': 'El campo Fecha inicial es requerido'})
--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 class UserForm(forms.Form):
-    name = forms.CharField(max_length=200, error_messages={'required': 'El campo usuario es requerido', 'max_length': 'El campo usuario debe tener una longitud maxima de 200 caracteres'})
+    alphabetic = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters are allowed.')
+
+    name = forms.CharField(max_length=200, validators=[alphabetic], error_messages={'required': 'El campo usuario es requerido', 'max_length': 'El campo usuario debe tener una longitud maxima de 200 caracteres'})
     password = forms.CharField(max_length=200, error_messages={'required': 'El campo contraseña es requerido', 'max_length': 'El campo contraseña debe tener una longitud maxima de 200 caracteres'})
