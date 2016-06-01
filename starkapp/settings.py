b577d2ce36dee35b112caa0a243e1724d2b6588c
Igor Roldan <a20121487@pucp.pe>
2016-06-01 16:46:21 -0500

    todo

--- starkapp/settings.py
+++ starkapp/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'starkapp',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'alushta65',
         'HOST': '127.0.0.1',
         'PORT': '',
     }
