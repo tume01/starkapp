5a8b49caa0e92b5fd691c7f38e039a6c35ebc050
jose <jpereira@pucp.pe>
2016-06-03 01:02:03 -0500

    Pass
    Regrese el pass al original

--- starkapp/settings.py
+++ starkapp/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'starkapp',
         'USER': 'root',
-        'PASSWORD': '1111',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '',
     }
