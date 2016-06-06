8d86cbea53423e1794196b62c1537e1641284920
jose <jpereira@pucp.pe>
2016-06-06 00:17:29 -0500

    Ajaxs
    Verificacion de usuario y editar usuario

--- static/assets/js/Admin/Membership/member_forms_validations.js
+++ static/assets/js/Admin/Membership/member_forms_validations.js
@@ -1,6 +1,7 @@
 var MemberFormValidation = function() {
     // Init Bootstrap Forms Validation, for more examples you can check out https://github.com/jzaefferer/jquery-validation
-    var initValidationBootstrap = function(){
+    var initValidationBootstrap = function(){    
+
         jQuery('.js-validation-bootstrap').validate({
             ignore: ['cancel'],
             errorClass: 'help-block animated fadeInDown',
@@ -25,17 +26,44 @@ var MemberFormValidation = function() {
                     required: true,
                     maxlength: 200
                 },
-                'surname': {
+                'paternalLastName': {
                     required: true,
                     maxlength:200
+                    //email: true
                 },
-                'dni': {
-                    required: true                    
-                },                                
+                'maternalLastName': {
+                    required: true,
+                    maxlength:200
+                    //email: true
+                },
+                'num_doc': {
+                    required: true,    
+                    remote: {
+                        url: url,
+                        type: "post",
+                        data: {
+                            username: function() {
+                                return $( "#num_doc" ).val();
+                            }, 'csrfmiddlewaretoken': CSRF_TOKEN, 
+                            id_member: function() {
+                                return $( "#id" ).val();
+                            }
+                        }
+                    }                           
+                },                              
                 'address': {
                     required: true,
                     maxlength:200
                 },
+                'department': {
+                     required: true,                             
+                },  
+                 'province': {
+                     required: true,                           
+                },  
+                 'district': {
+                     required: true,                           
+                },  
                 'phone': {
                     required: true,
                     minlength:7
@@ -44,6 +72,12 @@ var MemberFormValidation = function() {
                 'email': {
                     required: true,
                     email: true
+                },
+                'initialDate': {
+                    required: true
+                },
+                'finalDate':{
+                    required:true
                 }
             },
             messages: {
@@ -51,16 +85,30 @@ var MemberFormValidation = function() {
                     required: 'Por favor ingrese un nombre',
                     maxlength: 'El nombre no puede tener más de 200 caracteres'
                 },
-                'surname': {
-                    required: 'Por favor ingrese un apellido',                    
-                    maxlength: 'El apellido no puede tener más de 200 caracteres'
+                'paternalLastName': {
+                    required: 'Por favor ingrese un apellido',
+                    maxlength: 'El apellido debe tener máximo 200 caracteres'                    
                 },
-                'dni': {
-                    required: 'Por favor ingrese un dni'                    
-                },                
+                'maternalLastName': {
+                    required: 'Por favor ingrese un apellido',
+                    maxlength: 'El apellido debe tener máximo 200 caracteres'                    
+                },
+                'num_doc': {
+                    required: 'Por favor ingrese un dni' ,
+                    remote: 'Este documento ya esta en uso'                   
+                },              
                 'address': {
                     required: 'Por favor ingrese una dirección',
                     maxlength: 'La dirección no puede tener más de 200 caracteres'
+                },
+                'department': {
+                     required: "Por favor seleccione un departamento",                             
+                },  
+                 'province': {
+                     required: "Por favor seleccione una provincia",                           
+                },  
+                 'district': {
+                     required: "Por favor seleccione un distrito",                           
                 },
                 'phone': {
                     required: 'Por favor ingrese un teléfono',   
@@ -70,6 +118,12 @@ var MemberFormValidation = function() {
                 'email': {
                     required:'Por favor ingrese un email',
                     email:'Por favor ingrese un email válido'                
+                },
+                'initialDate': {
+                    required: 'Por favor ingrese una fecha inicial'
+                },
+                'finalDate':{
+                    required: 'Por favor ingrese una fech final'
                 }                
             }
         });
@@ -77,6 +131,7 @@ var MemberFormValidation = function() {
 
     return {
         init: function () {
+
             // Init Bootstrap Forms Validation
             initValidationBootstrap();
 
