bd9ee589294108b22442792ddfc8a70e5369fe0c
fabioolcese <a20110066@pucp.pe>
2016-06-25 19:59:40 -0400

    cambios de solicitud, miembro y afiliado[sin probar]
    yolo 420 1337 (tenemos copia de seguridad)

--- affiliate/models.py
+++ affiliate/models.py
@@ -12,7 +12,7 @@ class Relationship(models.Model):
 class Affiliate(models.Model):
     member = models.ForeignKey(x.Member, on_delete=models.CASCADE)
     identity_document_type = models.ForeignKey(z.Identity_Document_Type, on_delete=models.CASCADE)
-    ubigeo = models.ForeignKey(y.Ubigeo, on_delete=models.CASCADE)
+    ubigeo = models.ForeignKey(y.Ubigeo, related_name='affiliate_address_ubigeo', on_delete=models.CASCADE)
     name=models.TextField(max_length=200)
     paternalLastName=models.TextField(max_length=200)
     maternalLastName=models.TextField(max_length=200)
@@ -22,3 +22,16 @@ class Affiliate(models.Model):
     email=models.TextField(max_length=200)
     state=models.IntegerField()
     relationship = models.ForeignKey(Relationship, on_delete=models.CASCADE)
+
+    gender = models.IntegerField()  # 0 masculino 1 femenino
+    nationality = models.TextField(max_length=20)
+    birthDate = models.DateField()
+    birthPlace = models.TextField(max_length=200)
+    photo = models.ImageField(upload_to='affiliate')
+    specialization = models.TextField(max_length=200, blank=True, null=True)
+    workPlace = models.TextField(max_length=200, blank=True, null=True)
+    workPlaceJob = models.TextField(max_length=200, blank=True, null=True)
+    workPlacePhone = models.IntegerField(blank=True, null=True)
+    birthUbigeo = models.ForeignKey(y.Ubigeo, related_name='affiliate_birth_ubigeo', on_delete=models.CASCADE)
+    maritalStatus = models.TextField(max_length=20)
+    cellphoneNumber = models.IntegerField()
--- membership_application/models.py
+++ membership_application/models.py
@@ -1,6 +1,7 @@
 from django.db import models as m
 from memberships import models as x
 from identity_document_type import models as y
+from ubigeo import models as z
 
 # Create your models here.
 class Membership_Application(m.Model):
@@ -13,5 +14,41 @@ class Membership_Application(m.Model):
     comments = m.CharField(max_length=200)
     initialDate = m.DateField()
     finalDate = m.DateField()
+
+    photo = m.ImageField(upload_to='membership_application')
+    address = m.TextField(max_length=200)
+    gender = m.IntegerField() #0 masculino 1 femenino
+    workPlace = m.TextField(max_length=200)
+    workPlaceJob = m.TextField(max_length=200)
+    workPlacePhone = m.IntegerField()
+    nationality = m.TextField(max_length=20)
+    maritalStatus = m.TextField(max_length=20)
+    cellphoneNumber = m.IntegerField()
+    specialization = m.TextField(max_length=200)
+    birthDate = m.DateField()
+    birthPlace = m.TextField(max_length=200)
+    email=m.TextField(max_length=200)
+    phone=m.IntegerField()
+    ubigeo = m.ForeignKey(z.Ubigeo, related_name='app_address_ubigeo', on_delete=m.CASCADE)
+    birthUbigeo = m.ForeignKey(z.Ubigeo, related_name='app_birth_ubigeo', on_delete=m.CASCADE)
+
+    #conyuge
+    sidentity_document_type = m.ForeignKey(y.Identity_Document_Type, related_name='identity_doc_spouse', blank=True, null=True, on_delete=m.CASCADE)
+    sfirstName = m.CharField(max_length=20, blank=True, null=True)
+    spaternalLastName = m.CharField(max_length=20, blank=True, null=True)
+    smaternalLastName = m.CharField(max_length=20, blank=True, null=True)
+    sdocument_number = m.IntegerField(blank=True, null=True)
+    sgender = m.IntegerField(blank=True, null=True) #0 masculino 1 femenino
+    sspecialization = m.TextField(max_length=200, blank=True, null=True)
+    snationality = m.TextField(max_length=20, blank=True, null=True)
+    sbirthDate = m.DateField(blank=True, null=True)
+    sbirthPlace = m.TextField(max_length=200, blank=True, null=True)
+    sphoto = m.ImageField(upload_to='membership_application', blank=True, null=True)
+    sworkPlace = m.TextField(max_length=200, blank=True, null=True)
+    sworkPlaceJob = m.TextField(max_length=200, blank=True, null=True)
+    sworkPlacePhone = m.IntegerField(blank=True, null=True)
+    semail=m.TextField(max_length=200, blank=True, null=True)
+    sbirthUbigeo = m.ForeignKey(z.Ubigeo, blank=True, null=True,related_name='spouse_birth_ubigeo', on_delete=m.CASCADE)
+
     status = m.IntegerField()
     #0 = Eliminado, 1 = En espera, 2 = Aceptado, 3 = Rechazado
