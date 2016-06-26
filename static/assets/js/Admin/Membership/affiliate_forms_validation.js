e906f6449dccb6c649a8ae4fc2e65596da8bc005
Zephiross <a20121129@pucp.pe>
2016-06-26 13:59:25 -0500

    cambiar false por true

--- static/assets/js/Admin/Membership/affiliate_forms_validation.js
+++ static/assets/js/Admin/Membership/affiliate_forms_validation.js
@@ -132,7 +132,7 @@ var AffiliateFormValidation = function() {
                     required:true
                 },
                 'photo':{
-                    required:false
+                    required:true
                 }
             },
             messages: {
