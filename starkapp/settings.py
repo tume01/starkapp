83fe40f6dd3e8df34299a880a0b498d98e278611
Christian <christianbedonv@pucp.pe>
2016-07-01 07:36:27 -0500

    TIME_ZONE='CET' in the settings file

--- starkapp/settings.py
+++ starkapp/settings.py
@@ -144,14 +144,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Lima'
-
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
 
+TIME_ZONE='CET'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
